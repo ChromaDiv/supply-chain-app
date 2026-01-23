de05306bbb09c8cbe6aee75d59aac2fe10ad4cfb
Sohaib Latif <sohaiblatif@Sohaibs-MacBook-Air.local>
2026-01-23 17:03:16 +0500

    Fix: Moved backend to api folder and updated vercel.json

--- backend/passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Tell the server where the files are
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Import 'app' from main.py and rename it to 'application'
-# Phusion Passenger (Hostinger's server) looks for 'application'
-from main import app as application
