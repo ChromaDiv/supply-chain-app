ba6c468016358329fbf7cbed1bec32ba9e645bb6
Sohaib Latif <sohaiblatif@Sohaibs-MacBook-Air.local>
2026-01-24 01:35:22 +0500

    feat: setup Hostinger compatibility with a2wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,11 @@
 import sys, os
+from a2wsgi import ASGIMiddleware
+
+# Insert the project root in the path so we can import 'backend'
 sys.path.insert(0, os.path.dirname(__file__))
-from backend.index import app as application
+
+# Import the FastAPI 'app' from backend/index.py
+from backend.index import app as application_asgi
+
+# Wrap the ASGI app with ASGIMiddleware to make it WSGI compatible
+application = ASGIMiddleware(application_asgi)
